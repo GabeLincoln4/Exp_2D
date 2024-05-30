8b3dabba9b8216d94dd4984c80adfa59a6f80842
Gabriel Gavin <ggavin2@uncc.edu>
2024-05-30 15:33:59 -0400

    Create Float variable class for scriptable objects and create scriptable object that hold player and enemy luck level and hp

---
+++ Assets/Scripts/FloatVariableObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu]
+public class FloatVariableObject : ScriptableObject
+{
+    public float _value;
+}
