9a37e311392815577d96d7cfb4c4c58b87f076c7
Gabriel Gavin <ggavin2@uncc.edu>
2024-06-05 14:04:22 -0400

    create game event interactions between character responses and timer to prototype a turn based system

---
+++ Assets/Scripts/PhysicalAttack.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalAttack : MonoBehaviour
+{
+    private void OnEnable () 
+    {
+        Debug.Log("Physical attack has been initiated.");
+    }
+}
