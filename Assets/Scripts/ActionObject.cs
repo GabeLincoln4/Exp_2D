8910b65d3152dddf6b5860e1885033d76c17e582
Gabriel Gavin <ggavin2@uncc.edu>
2024-07-17 13:05:29 -0400

    create action scriptable object

---
+++ Assets/Scripts/ActionObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu]
+public class ActionObject : ScriptableObject
+{
+    public string _actionName;
+    public string _actionValue;
+}
