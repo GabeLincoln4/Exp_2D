03895be9e1f8bca600ab4449236980a59b4402db
Gabriel Gavin <ggavin2@uncc.edu>
2024-07-17 14:53:04 -0400

    refactor by consolidating stats for characters into one scriptable object

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,26 +4,23 @@ using System.Collections;
 public class Character : MonoBehaviour
 {
     [SerializeField]
-    private FloatVariableObject _characterHP;
-
-    [SerializeField]
-    private FloatVariableObject _characterLuck;
+    private CharacterStatsObject _characterStats;
 
     [SerializeField]
     private float _initialLuck;
 
     private void OnEnable()
     {
-        _characterLuck._value = _initialLuck;
+        _characterStats._characterMental = _initialLuck;
     }
 
     private void OnDisable ()
     {
-        _characterLuck._value = _initialLuck;
+        _characterStats._characterMental = _initialLuck;
     }
     
-    public void DecrementLuck(float damageTaken)
+    public void DecrementMorale(float damageTaken)
     {
-        _characterLuck._value -= damageTaken;
+        _characterStats._characterMental -= damageTaken;
     }
 }
---
+++ Assets/Scripts/CharacterStatsObject.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu]
+public class CharacterStatsObject : ScriptableObject
+{
+    public string _name;
+    public float _characterHealth;
+    public float _characterMental;
+}
